c37abdb855bee3af1ce6ed13436f6da7be0c2b0d
Teodor Minca <mincateo@gmail.com>
2026-01-07 18:30:21 +0200

    Added implementation of main.cpp inside routing-service

--- routing-service/src/main.cpp
+++ routing-service/src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Routing Service C++ started..." << std::endl;
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        std::cout << "Service is alive (waiting for requests)..." << std::endl;
+    }
     return 0;
 }
